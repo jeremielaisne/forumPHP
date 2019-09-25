65d7afaa141f6b6b5cff0381e5d22d79c8b0b6fd
JerLai <jlai083@live.fr>
2019-09-25 18:12:17 +0200

    Debut test UserClass PHPUnit

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,9 +1,31 @@
 <?php
 
-class UserTest extends phpunit\Framework\TestCase {
-    
-    public function createUser()
+use PHPUnit\Framework\TestCase;
+use App\User;
+
+class UserTest extends TestCase 
+{
+    protected $user;
+
+    public function setUp()
     {
-        ;
+        if (null == $this->user)
+        {
+            $this->user = new User();
+        }
+    }
+
+    public function testSetterId()
+    {
+        $this->user->setId(1);
+        $this->assertSame(1, $this->user->getId(1));
+    }
+
+    public function testLoad()
+    {
+        self::testSetterId();
+        $this->assertIsObject($this->user->load());
+        $this->assertEquals('Laisné', $this->user->getLastname());
+        $this->assertEquals('Jérémie', $this->user->getFirstname());
     }
 }
